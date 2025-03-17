76c9a02b6906aac1a82c622b42a6d8a40022e154
Stanciu Alin <stanciualin98@yahoo.com>
2025-03-17 13:28:59 +0200

    Add project files.

---
+++ BookStoreApp.API/Program.cs
@@ -0,0 +1,64 @@
+using Microsoft.OpenApi.Models;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Book Store API",
+        Version = "v1",
+        Description = "API example with Swagger in ASP.NET Core",
+        Contact = new OpenApiContact
+        {
+            Name = "AMS",
+            Email = "ams@bookstore.com",
+            Url = new Uri("https://github.com/StanciuAlin")
+        }
+    });
+});
+
+// Serilog
+builder.Host.UseSerilog((ctx, lc) =>
+{
+    lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration);
+});
+
+// CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Book Store API v1");
+        c.RoutePrefix = string.Empty; // Make Swagger accesible from the root URL
+    });
+}
+
+app.UseCors("AllowAll");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
