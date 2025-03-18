fb610990c430f37a0f2b503130ca12bd1932a22b
Stanciu Alin <stanciualin98@yahoo.com>
2025-03-19 00:03:24 +0200

    Added Book API Controller

--- BookStoreApp.API/Configurations/AutorMapperConfig.cs
+++ BookStoreApp.API/Configurations/AutorMapperConfig.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using BookStoreApp.API.Data;
 using BookStoreApp.API.Models.Author;
+using BookStoreApp.API.Models.Book;
 
 namespace BookStoreApp.API.Configurations
 {
@@ -11,6 +12,15 @@ namespace BookStoreApp.API.Configurations
             CreateMap<Author, AuthorCreateDto>().ReverseMap();
             CreateMap<Author, AuthorReadOnlyDto>().ReverseMap();
             CreateMap<Author, AuthorUpdateDto>().ReverseMap();
+
+            CreateMap<Book, BookCreateDto>().ReverseMap();
+            CreateMap<Book, BookUpdateDto>().ReverseMap();
+            CreateMap<Book, BookReadOnlyDto>()
+                .ForMember(q => q.AuthorName, options => options.MapFrom(map => $"{map.Author.FirstName} {map.Author.LastName}"))
+                .ReverseMap();
+            CreateMap<Book, BookDetailsDto>()
+                .ForMember(q => q.AuthorName, options => options.MapFrom(map => $"{map.Author.FirstName} {map.Author.LastName}"))
+                .ReverseMap();
         }
     }
 }
